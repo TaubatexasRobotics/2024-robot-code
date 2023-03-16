03aa1bd66933b3afaf0effcd61081f76c4027c43
Flávio Lima <flavi@Flavio-note-W11>
2023-03-15 22:30:48 -0300

    feat: define controller inputs

---
+++ controller.py
@@ -0,0 +1,54 @@
+import wpilib
+
+JOYSTICK_PORT = 0
+
+A_BUTTON = 1
+B_BUTTON = 2
+X_BUTTON = 3
+Y_BUTTON = 4
+LB_BUTTON = 5
+RB_BUTTON = 6
+SELECT_BUTTON = 7
+START_BUTTON = 8
+L3_BUTTON = 9
+R3_BUTTON = 10
+
+AXIS_LEFT_X = 0
+AXIS_LEFT_Y = 1
+AXIS_RIGHT_X = 2
+AXIS_RIGHT_Y = 3
+AXIS_LEFT_TRIGGER = 4
+AXIS_RIGHT_TRIGGER = 5
+
+POV_UP = 0
+POV_UP_RIGHT = 45
+POV_RIGHT = 90
+POV_DOWN_RIGHT = 135
+POV_DOWN = 180
+POV_DOWN_LEFT = 225
+POV_LEFT = 270
+POV_UP_LEFT = 315
+
+class Controller:
+    def __init__(self):
+        self.stick = wpilib.Joystick(JOYSTICK_PORT)
+
+    def toggle_intake(self):
+        if self.stick.getRawButtonPressed(B_BUTTON) == True:
+            return True
+        return False
+    
+    def toggle_compressor(self):
+        if self.stick.getRawButtonPressed(SELECT_BUTTON) == True:
+            return True
+        return False
+    
+    def get_drive(self):
+        return self.stick.getRawAxis(1), self.stick.getRawAxis(0)
+    
+    def change_arm_angle(self):
+        return self.stick.getRawAxis(3)
+    
+    def change_arm_lenght(self):
+        return self.stick.getRawAxis(4)
+
--- robot.py
+++ robot.py
@@ -1,13 +1,13 @@
 from drivetrain import Drivetrain
 from arm import Arm
 from intake import Intake
+from controller import Controller
 
 import wpilib
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
-        self.stick = wpilib.Joystick(0)
-
+        self.controller = Controller()
         self.drivetrain = Drivetrain()
         self.arm = Arm()
         self.intake = Intake()
@@ -16,19 +16,16 @@ class MyRobot(wpilib.TimedRobot):
         self.drivetrain.differential_drive.setSafetyEnabled(True)
 
     def teleopPeriodic(self) -> None:
-        self.drivetrain.differential_drive.arcadeDrive(
-            self.stick.getRawAxis(1),
-            self.stick.getRawAxis(0),
-        )
+        self.drivetrain.differential_drive.arcadeDrive( *self.controller.get_drive() )
 
         if not self.arm.is_homed:
             self.arm.home_arm()
             return
 
-        if self.stick.getRawButtonPressed(7) == True:
+        if self.controller.toggle_compressor():
             self.intake.toggle_compressor()
         
-        if self.stick.getRawButtonPressed(2) == True:
+        if self.controller.toggle_intake():
             self.intake.toggle_intake()
         
     def autonomousPeriodic(self) -> None:
