34cb4006fe3748ba06c9f42f243a2462f7a63b15
Flávio de Lima <flaviolimpio@gmail.com>
2024-02-27 07:26:52 -0300

    separate joystick profiles to new file

--- controller.py
+++ controller.py
@@ -1,36 +1,10 @@
 import wpilib
+from joystick_profiles import joysticks
+joysticks_profile = 'xbox_controller'
+controller = joysticks[joysticks_profile]
 
 JOYSTICK_PORT = 0
 LOW_SENSITIVITY_FACTOR = 0.6
-
-A_BUTTON = 1
-B_BUTTON = 2
-X_BUTTON = 3
-Y_BUTTON = 4
-LB_BUTTON = 5
-RB_BUTTON = 6
-SELECT_BUTTON = 7
-START_BUTTON = 8
-L3_BUTTON = 9
-R3_BUTTON = 10
-
-AXIS_LEFT_X = 0
-AXIS_LEFT_Y = 1
-AXIS_RIGHT_X = 4
-AXIS_RIGHT_Y = 5
-AXIS_LEFT_TRIGGER = 2 #- XBOX CONTROLLER
-AXIS_RIGHT_TRIGGER = 3 #- # XBOX CONTROLLER
-# AXIS_LEFT_TRIGGER = 2 - Playstation
-# AXIS_RIGHT_TRIGGER = 5
-
-POV_UP = 0
-POV_UP_RIGHT = 45
-POV_RIGHT = 90
-POV_DOWN_RIGHT = 135
-POV_DOWN = 180
-POV_DOWN_LEFT = 225
-POV_LEFT = 270
-POV_UP_LEFT = 315
 
 class Controller:
     def __init__(self):
@@ -43,52 +17,52 @@ class Controller:
         return 1.0
     
     def get_drive(self) -> tuple[float, float]:
-        rt_value = self.stick.getRawAxis(AXIS_RIGHT_TRIGGER)
-        lt_value = self.stick.getRawAxis(AXIS_LEFT_TRIGGER)
+        rt_value = self.stick.getRawAxis(controller['AXIS_RIGHT_TRIGGER'])
+        lt_value = self.stick.getRawAxis(controller['AXIS_LEFT_TRIGGER'])
         combined_value = lt_value - rt_value
         
         forward_speed = combined_value * self.sensitivity_factor()
-        rotation_speed = self.stick.getRawAxis(AXIS_LEFT_X) * self.sensitivity_factor()
+        rotation_speed = self.stick.getRawAxis(controller['AXIS_LEFT_X']) * self.sensitivity_factor()
 
         return forward_speed , rotation_speed
     
     def move_angle(self) -> float:
-        return self.stick.getRawAxis(AXIS_RIGHT_Y)
+        return self.stick.getRawAxis(controller['AXIS_RIGHT_Y'])
     
     def decrease_arm_angle(self) -> bool:
-        return self.stick.getRawButton(Y_BUTTON)
+        return self.stick.getRawButton(controller['Y_BUTTON'])
     
     def increase_arm_angle(self) -> bool:
-        return self.stick.getRawButton(A_BUTTON)
+        return self.stick.getRawButton(controller['A_BUTTON'])
     
     def stop_arm_angle(self) -> bool:
-        return self.stick.getRawButtonReleased(A_BUTTON) or self.stick.getRawButtonReleased(Y_BUTTON)
+        return self.stick.getRawButtonReleased(controller['A_BUTTON']) or self.stick.getRawButtonReleased(controller['Y_BUTTON'])
     
     def catch_gamepiece(self) -> bool:
-        return self.stick.getRawButton(X_BUTTON)
+        return self.stick.getRawButton(controller['X_BUTTON'])
     
     def store_gamepiece(self) -> bool:
-        return self.stick.getRawAxis(AXIS_RIGHT_X) < -0.2
+        return self.stick.getRawAxis(controller['AXIS_RIGHT_X']) < -0.2
     
     def send_to_shoot(self) -> bool:
-        print(self.stick.getRawAxis(AXIS_RIGHT_X) > 0.2)
-        return self.stick.getRawAxis(AXIS_RIGHT_X) > 0.2
+        print(self.stick.getRawAxis(controller['AXIS_RIGHT_X']) > 0.2)
+        return self.stick.getRawAxis(controller['AXIS_RIGHT_X']) > 0.2
     
     def release_gamepiece(self) -> bool:
-        return self.stick.getRawButton(B_BUTTON)
+        return self.stick.getRawButton(controller['B_BUTTON'])
     
     def stop_intake(self) -> bool:
-        return self.stick.getRawButtonReleased(B_BUTTON) or self.stick.getRawButtonReleased(X_BUTTON)
+        return self.stick.getRawButtonReleased(controller['B_BUTTON']) or self.stick.getRawButtonReleased(controller['X_BUTTON'])
 
     def sensitivity_toggle_button(self) -> bool:
-        return self.stick.getRawButtonPressed(SELECT_BUTTON)
+        return self.stick.getRawButtonPressed(controller['SELECT_BUTTON'])
     
     def toggle_low_sensitivity_mode(self) -> bool:
         self.low_sensitivity_mode = not self.low_sensitivity_mode
 
     def climb_up(self) -> bool:
-        return self.stick.getRawButton(RB_BUTTON)
+        return self.stick.getRawButton(controller['RB_BUTTON'])
 
     def climb_down(self) -> bool:
-        return self.stick.getRawButton(LB_BUTTON)
+        return self.stick.getRawButton(controller['LB_BUTTON'])
     
---
+++ joystick_profiles.py
@@ -0,0 +1,58 @@
+
+joysticks = {
+    'xbox_controller' : {
+        'AXIS_LEFT_X': 0,
+        'AXIS_LEFT_Y': 1,
+        'AXIS_LEFT_TRIGGER': 2,
+        'AXIS_RIGHT_TRIGGER': 3,
+        'AXIS_RIGHT_X': 4,
+        'AXIS_RIGHT_Y': 5,
+
+        'POV_UP': 0,
+        'POV_UP_RIGHT': 45,
+        'POV_RIGHT': 90,
+        'POV_DOWN_RIGHT': 135,
+        'POV_DOWN': 180,
+        'POV_DOWN_LEFT': 225,
+        'POV_LEFT': 270,
+        'POV_UP_LEFT': 315,
+
+        'A_BUTTON': 1,
+        'B_BUTTON': 2,
+        'X_BUTTON': 3,
+        'Y_BUTTON': 4,
+        'LB_BUTTON': 5,
+        'RB_BUTTON': 6,
+        'SELECT_BUTTON': 7,
+        'START_BUTTON': 8,
+        'L3_BUTTON': 9,
+        'R3_BUTTON': 10,
+    },
+    'playstation_dualsense_controller' : {
+        'AXIS_LEFT_TRIGGER': 2,
+        'AXIS_RIGHT_TRIGGER': 5,
+
+        'AXIS_LEFT_X': 0,
+        'AXIS_LEFT_Y': 1,
+
+        'POV_UP': 0,
+        'POV_UP_RIGHT': 45,
+        'POV_RIGHT': 90,
+        'POV_DOWN_RIGHT': 135,
+        'POV_DOWN': 180,
+        'POV_DOWN_LEFT': 225,
+        'POV_LEFT': 270,
+        'POV_UP_LEFT': 315,
+
+        'A_BUTTON': 1,
+        'B_BUTTON': 2,
+        'X_BUTTON': 3,
+        'Y_BUTTON': 4,
+        'LB_BUTTON': 5,
+        'RB_BUTTON': 6,
+        'SELECT_BUTTON': 7,
+        'START_BUTTON': 8,
+        'L3_BUTTON': 9,
+        'R3_BUTTON': 10,
+    }
+}
