e45b8e99f5b6ce3b16f6d383d358cfc358b7d410
Flávio de Lima <flaviolimpio@gmail.com>
2024-02-29 03:19:34 -0300

    fix axis_value function calls

--- controller.py
+++ controller.py
@@ -9,6 +9,7 @@ class Controller:
     def __init__(self, joystick_port=0):
         self.stick = wpilib.Joystick(joystick_port)
         self.low_sensitivity_mode = False
+        self.controller = controller
 
     def is_pressed(self, button_name: str) -> bool:
         return self.stick.getRawButtonPressed(controller[button_name])
@@ -43,17 +44,19 @@ class Controller:
         button_states = [self.is_held(button_name) for button_name in button_names]
         return any(button_states)
     
+    def axis_value(self, axis_name:str) -> bool:
+        axis_value = self.stick.getRawAxis(self.controller[axis_name])
+        return axis_value
+    
     def axis_to_digital(self, axis_name: str, threshold: float) -> bool:
         if threshold < 0:
-            return self.axis_value(controller[axis_name]) < threshold
-        return self.axis_value(controller[axis_name]) > threshold
+            return self.axis_value(axis_name) < threshold
+        return self.axis_value(axis_name) > threshold
     
     def axis_between(self, axis_name: str, lower_threshold: float, upper_threshold: float) -> bool:
-        axis_value = self.axis_value(controller[axis_name])
+        axis_value = self.axis_value(axis_name)
         return axis_value > lower_threshold and axis_value < upper_threshold
     
-    def axis_value(self, axis_name:str) -> bool:
-        return self.stick.getRawAxis(controller[axis_name])
     
     def sensitivity_factor(self) -> float:
         if self.low_sensitivity_mode == True:
@@ -61,14 +64,15 @@ class Controller:
         return 1.0
     
     def get_drive(self) -> tuple[float, float]:
-        rt_value = self.axis_value(controller['AXIS_RIGHT_TRIGGER'])
-        lt_value = self.axis_value(controller['AXIS_LEFT_TRIGGER'])
+        rt_value = self.axis_value('AXIS_RIGHT_TRIGGER')
+        lt_value = self.axis_value('AXIS_LEFT_TRIGGER')
+        
         combined_value = lt_value - rt_value
 
         sensitivity_factor = self.sensitivity_factor()
         
         forward_speed = combined_value * sensitivity_factor
-        rotation_speed = self.axis_value(controller['AXIS_LEFT_X']) * sensitivity_factor
+        rotation_speed = self.axis_value('AXIS_LEFT_X') * sensitivity_factor
 
         return forward_speed , rotation_speed
     
